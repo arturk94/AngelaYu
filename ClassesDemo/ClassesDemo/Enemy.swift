17f36c5380a9b95c03a806b407856a552cca12ac
Artur KNOTHE <artur@knothe.org>
2025-04-05 19:48:20 +0200

    Adding ClassesDemo

---
+++ ClassesDemo/ClassesDemo/Enemy.swift
@@ -0,0 +1,13 @@
+
+class Enemy {
+    var health = 100
+    var attackStrength = 10
+    
+    func move() {
+        print("Walk")
+    }
+    
+    func attack() {
+        print("Land a hoit, does \(attackStrength) damage")
+    }
+}
---
+++ ClassesDemo/ClassesDemo/main.swift
@@ -0,0 +1,12 @@
+
+let skeleton = Enemy()
+
+
+let dragon = Dragon()
+dragon.wingSpan = 5
+dragon.attackStrength = 15
+dragon.talk(speech: "My teeth are swords! My claws are spears! My wings are hurricane!")
+dragon.move()
+dragon.attack()
+
+
