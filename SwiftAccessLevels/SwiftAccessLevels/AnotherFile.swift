c10c81c252e39c4ce535c2c230a51510de85af82
Artur KNOTHE <artur@knothe.org>
2025-04-05 20:03:10 +0200

    Adding SwiftAccess

---
+++ SwiftAccessLevels/SwiftAccessLevels/AnotherFile.swift
@@ -0,0 +1,23 @@
+//
+//  AnotherFile.swift
+//  SwiftAccessLevels
+//
+//  Created by Angela Yu on 14/09/2018.
+//  Copyright © 2018 London App Brewery. All rights reserved.
+//
+
+import Foundation
+
+class AnotherClassInAnotherFile {
+    
+    init() {
+        let aClass = AClass()
+        
+        //Step 8. Try to print aFilePrivateProperty Here - Not Possible
+        //print("\(aClass.aFilePrivateProperty) printed from init in AnotherClassInAnotherFile")
+        
+        //Step 10. Try to print anInternalProperty Here - Possible
+        //print("\(aClass.anInternalProperty) printed from init in AnotherClassInAnotherFile")
+    }
+    
+}
