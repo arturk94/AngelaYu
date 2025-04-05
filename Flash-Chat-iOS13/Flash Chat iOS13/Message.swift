c27d219bd5e1bac1202bdab32445cd2659edb59a
Artur KNOTHE <artur@knothe.org>
2025-04-05 19:52:28 +0200

    Flash-Chat-iOS13

---
+++ Flash-Chat-iOS13/Flash Chat iOS13/Message.swift
@@ -0,0 +1,14 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by Artur KNOTHE on 25/04/2020.
+//  Copyright © 2020 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    let sender: String //artur@knothe.org
+    let body: String
+}
